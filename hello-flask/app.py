0752d55b4affc56ee8cdff692228bc0a26fa9e55
unknown <zhaojf85@163.com>
2015-12-28 12:20:05 +0800

    add sockt get host

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import socket
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!\n'
+    return 'Hello, world!\n'.socket.gethostname()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
